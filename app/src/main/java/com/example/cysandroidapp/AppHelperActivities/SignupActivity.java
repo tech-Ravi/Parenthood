8dd765238a227d4c833e3417bdcf9f958fcaa1ba
ravi-prakash-gortnm <ravi.prakash@gortnm.in>
2021-09-20 19:54:44 +0530

    Home page design added

--- app/src/main/java/com/example/cysandroidapp/AppHelperActivities/SignupActivity.java
+++ app/src/main/java/com/example/cysandroidapp/AppHelperActivities/SignupActivity.java
@@ -1,12 +1,12 @@
-package com.example.cysandroidapp;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.cysandroidapp.AppHelperActivities;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.cysandroidapp.R;
 
 public class SignupActivity extends Activity {
     Button btnSignup;
--- app/src/main/java/com/example/cysandroidapp/AppHelperActivities/SplashScreenActivity.java
+++ app/src/main/java/com/example/cysandroidapp/AppHelperActivities/SplashScreenActivity.java
@@ -1,18 +1,11 @@
-package com.example.cysandroidapp;
-
-import android.annotation.SuppressLint;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.cysandroidapp.AppHelperActivities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.WindowInsets;
+
+import com.example.cysandroidapp.R;
 
 public class SplashScreenActivity extends Activity {
     @Override
